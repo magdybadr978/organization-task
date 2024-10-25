22ff7b560d3b5137ae614f8ca7aa8226b4ed71f2
magdybadr978 <magdibadr2002@gmail.com>
2024-10-25 21:57:48 +0300

    fix : UserModule

--- src/userDashboard/organization/organization.modules.ts
+++ src/userDashboard/organization/organization.modules.ts
@@ -8,9 +8,11 @@ import { OrganizationController } from './organization.controller';
 import { OrganizationService } from './organization.service';
 import { OrganizationRepository } from 'src/models/organization/organization.repository';
 import { UserMongoModule } from 'src/shared/modules/user-mongo.module';
+import { UserRepository } from 'src/models/user/user.repository';
+import { UserService } from '../user/user.service';
 
 @Module({
-  imports: [ MongooseModule.forFeature([{ name : Organization.name ,schema : organizationSchema}]) , OrganizationModule,UserMongoModule],
+  imports: [ MongooseModule.forFeature([{ name : Organization.name ,schema : organizationSchema}]),UserMongoModule],
   controllers: [OrganizationController],
   providers: [OrganizationService,OrganizationRepository,AuthService,JwtService,RolesGuard],
   exports: [OrganizationService , OrganizationRepository],
