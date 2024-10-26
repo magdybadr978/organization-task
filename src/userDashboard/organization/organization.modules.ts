d3135f022cd8437b3fd1a628f3f33c0430825bfc
magdybadr978 <magdibadr2002@gmail.com>
2024-10-26 12:44:54 +0300

    fix : path

--- src/userDashboard/organization/organization.modules.ts
+++ src/userDashboard/organization/organization.modules.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AuthService } from 'src/Guards/Auth.service';
+import { AuthService } from 'src/Guards/auth.service';
 import { RolesGuard } from 'src/Guards/Authorization';
+import { OrganizationRepository } from 'src/models/organization/organization.repository';
 import { Organization, organizationSchema } from 'src/models/organization/organization.schema';
+import { UserMongoModule } from 'src/shared/modules/user-mongo.module';
 import { OrganizationController } from './organization.controller';
 import { OrganizationService } from './organization.service';
-import { OrganizationRepository } from 'src/models/organization/organization.repository';
-import { UserMongoModule } from 'src/shared/modules/user-mongo.module';
-import { UserRepository } from 'src/models/user/user.repository';
-import { UserService } from '../user/user.service';
 
 @Module({
   imports: [ MongooseModule.forFeature([{ name : Organization.name ,schema : organizationSchema}]),UserMongoModule],
